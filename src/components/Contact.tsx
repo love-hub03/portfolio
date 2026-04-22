68b11997c7958e07873762699e663a15085fd4c0
love-hub03 <sainilove2203@gmail.com>
2026-04-22 18:10:24 +0530

    navbar fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,14 +10,14 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Email</h4>
             <p>
-              <a href="mailto:example@mail.com" data-cursor="disable">
-                example@mail.com
+              <a href="mailto:sainilove2203@gmail.com" data-cursor="disable">
+                sainilove2203@gmail.com
               </a>
             </p>
             <h4>Phone</h4>
             <p>
-              <a href="tel:+9199999999" data-cursor="disable">
-                +91 99999 99999
+              <a href="tel:+9179734704xx" data-cursor="disable">
+                +91 79734704xx
               </a>
             </p>
           </div>
@@ -58,10 +58,10 @@ const Contact = () => {
           </div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Moncy Yohannan</span>
+               Developed <br /> by <span>Lovepreet Saini</span>
             </h2>
             <h5>
-              <MdCopyright /> 2024
+              <MdCopyright /> 2026 All rights reserved.
             </h5>
           </div>
         </div>
